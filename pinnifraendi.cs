7a193e2ae3cd4bdbd32fcc4792587986638900bd
OrlandoMr555 <168282591+OrlandoMr555@users.noreply.github.com>
2024-05-04 12:39:38 -0400

    64373

---
+++ pinnifraendi.cs
@@ -0,0 +1,6 @@
+int pinLongitud = int.Parse(Console.ReadLine());
+int contLargo = 0;
+string resultado = "";
+
+double probability = Math.Pow(0.1, pinLongitud);
+Console.WriteLine("La probabilidad de que la clave se depita es de : " + probability);

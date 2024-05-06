5f07d6731ac6af817b10f3d3e17d0b3b46dcf43a
Thaliansb <thaliansosab@gmail.com>
2024-05-06 16:56:48 -0400

    Hola

---
+++ kattis/heliocentric.py
@@ -0,0 +1,16 @@
+import sys
+
+def numDays(e, m):
+    days = 0
+    while True:
+        if (e + days) % 365 == (m + days) % 687:
+            return days
+        days += 1
+
+ans = []
+for line in sys.stdin:
+    e, m = map(int, line.split())
+    ans.append(numDays(e, m))
+
+for i, a in enumerate(ans):
+    print(f"Case {i + 1}: {a}")

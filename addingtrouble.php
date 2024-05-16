69f5d811f8a0f5636df7ee16a53c263c4b86ea41
Didiego13 <152827649+Didiego13@users.noreply.github.com>
2024-05-16 00:48:36 -0400

    Add files via upload

---
+++ addingtrouble.php
@@ -0,0 +1,17 @@
+<?php
+
+function suma($a, $b, $c) {
+  if ($a + $b == $c) {
+    return "correct!";
+  } else {
+    return "wrong!";
+  }
+}
+
+$entrada = explode(" ", fgets(STDIN));
+$a = intval($entrada[0]);
+$b = intval($entrada[1]);
+$c = intval($entrada[2]);
+
+$resultado = suma($a, $b, $c);
+echo $resultado;
